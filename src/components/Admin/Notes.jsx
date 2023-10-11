0fe2aef562b3eee6a3c75eb83ae7c56424311e2c
Varun Singh <varunsingh9114@gmail.com>
2023-10-11 20:01:45 +0530

    added recoil atom

--- src/components/Admin/Notes.jsx
+++ src/components/Admin/Notes.jsx
@@ -1,10 +1,39 @@
 import React, { useEffect } from 'react'
 import api from '../../api/api.js'
 import { Typography } from '@mui/material'
+import adminnoteState from '../../store/atom/adminNote.jsx'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 function Notes() {
 
+    const notes=useRecoilValue(adminnoteState);
+    const setNotes=useSetRecoilState(adminnoteState);
+    useEffect(()=>{
+        async function getNotes(){
+            try{
+                const resp=await api.get("/admin/notes",{
+                    headers:{
+                        "Authorization":"Bearer "+localStorage.getItem("token")
+                    }
+                    
+                })
+                    if(resp.status==200)
+                    {
+                        setNotes([...resp.data]);
+                    }
 
-    
+            }catch(error)
+            {
+                console.log(error)
+            }
+
+        }
+        getNotes();
+    },[])
+
+    const data=notes.map((note)=>{
+        
+    })
+
   return (
     <div>
         <Typography variant='h2'> collection of my notes</Typography>
---
+++ src/store/atom/adminNote.jsx
@@ -0,0 +1,8 @@
+import { atom } from "recoil";
+
+const adminnoteState=atom({
+    key:"adminnoteState",
+    default:[]
+})
+
+export default adminnoteState;
